396df0d40a35aadf9a16d7c114270ecf4943c096
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-11-07 23:18:06 +0500

    changed database conf

--- config/settings/server.py
+++ config/settings/server.py
@@ -8,8 +8,8 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'irrigatsiya',
-        'USER': 'postgres',
+        'NAME': 'istombroker',
+        'USER': 'itom',
         "PASSWORD": "1",
         "HOST": "localhost",
         "PORT": 5432,
