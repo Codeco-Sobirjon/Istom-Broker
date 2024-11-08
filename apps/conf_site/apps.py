8327f4f3ee6d4fe2c24ef34835273a1325de40c6
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-11-08 16:13:58 +0500

    finished

--- apps/conf_site/apps.py
+++ apps/conf_site/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ConfSiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.conf_site'
+    verbose_name = "Сайт настройки"
