064c31c54677f51b990279ba56251ebd89679f64
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-11-13 13:34:38 +0500

    000

--- config/urls.py
+++ config/urls.py
@@ -40,4 +40,4 @@ urlpatterns += [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT,},),]
+# urlpatterns += [re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT,},),]
